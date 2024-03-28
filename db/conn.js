478a590476eb5ce5295d19b4a819ceaaec044cc7
GuilhermeSilva236 <ggui2561@gmail.com>
2024-03-27 21:24:59 -0300

    ultimo codigo aula

--- db/conn.js
+++ db/conn.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose')
 
 async function main(){
-
     try {
-    await mongoose.connect(`mongodb+srv://${process.env.DB_MONGO_USER}:${process.env.DB_MONGO_PWD}@cluster0.jpof0et.mongodb.net/?retryWrites=true&w=majority`)
-    console.log('Banco OK');
+        await mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.SENHA}@cluster0.npecx3c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+        console.log('Banco OK');
+        
+    } catch (error) {
+        console.log('Erro:'+ error);
+        
+    }
     
-} catch (error) {
-    console.log('Erro: ' + error)
 }
-
-}
-
 module.exports = main
